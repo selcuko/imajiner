b9901a20f5112c00a9e3db6ea041e63ef9cceb13
divisia <omrfyyz@gmail.com>
2020-07-23 14:32:27 +0300

    narrative detail view

--- imajiner/urls.py
+++ imajiner/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from notebook.views import NarrativeViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('hikaye/<slug:slug>/', NarrativeViews.Detail.as_view())
 ]
 
 if settings.DEBUG:
--- notebook/views.py
+++ notebook/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Narrative
 
-# Create your views here.
+
+class NarrativeViews:
+    class Detail(DetailView):
+        models = Narrative
+        context_object_name = 'narrative'
+        template_name = 'notebook/narrative/detail.html'
+
+        def get_queryset(self):
+            slug = self.kwargs['slug']
+            return Narrative.objects.get(slug=slug)
