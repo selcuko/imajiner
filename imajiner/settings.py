5fcdfd41a51253d92ec1df2242918943d11ff8be
divisia <omrfyyz@gmail.com>
2020-07-23 14:10:26 +0300

    configured tagmanager as an app

--- imajiner/settings.py
+++ imajiner/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notebook.apps.NotebookConfig',
+    'tagmanager.apps.TagManagerConfig',
 ]
 
 MIDDLEWARE = [
--- tagmanager/apps.py
+++ tagmanager/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class TagmanagerConfig(AppConfig):
+class TagManagerConfig(AppConfig):
     name = 'tagmanager'
