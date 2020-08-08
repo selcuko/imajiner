ba83da24b5fa422c5236cf786af912f3ba290349
divisia <omrfyyz@gmail.com>
2020-08-08 23:27:06 +0300

    sound record

--- notebook/models.py
+++ notebook/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from tagmanager.models import TagManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,6 +9,29 @@ from django.utils import text, html
 from uuid import uuid1 as uuid
 
 
+def ext_validator(file):
+        valid = ['mp3', 'aac', 'wav']
+        if file.name.split('.')[-1] not in valid:
+            raise ValidationError('File extension invalid. Options:', str(valid))
+
+class SoundRecord(models.Model):
+    file = models.FileField(
+        upload_to='voice-records',
+        validators=[ext_validator]
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=128, null=True, blank=True)
+    uploader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    
+    def __str__(self):
+        return f'Voice record file {self.file.name} at {self.uploaded_at}'
+
+    def save(self, *args, **kwargs):
+        if not self.uploader: raise Exception('SoundRecord with no uploader user.')
+        super().save(*args, **kwargs)
+
+
 class Narrative(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, null=True, unique=True)
@@ -15,6 +39,7 @@ class Narrative(models.Model):
     html = models.TextField(null=True)
     sketch = models.BooleanField(default=False)
     uuid = models.UUIDField()
+    sound = models.OneToOneField(SoundRecord, null=True, blank=True, on_delete=models.SET_NULL)
 
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='narratives', null=True)
