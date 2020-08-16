74e825eee94b3baf31a1509ce5a1ade1258b595c
divisia <omrfyyz@gmail.com>
2020-08-16 20:37:56 +0300

    author  page basics

--- identity/views.py
+++ identity/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .models import *
 
 def author_view(request, username):
-    try:
-        u = User.objects.get(username=username)
-    except User.DoesNotExist:
-        u = None
-    return render(request, 'identity/author.html', {'author': u})
+    user = User.objects.get(username=username)
+    return render(request, 'identity/author.html', {'user': user})
+
