e525d7f4f265f00e6ba5693c18fc3348d6a2e026
divisia <omrfyyz@gmail.com>
2020-08-16 20:11:08 +0300

    ?next redirects

--- gatewall/static/shadow.js
+++ gatewall/static/shadow.js
@@ -1,7 +1,13 @@
 const $shadowButton = document.getElementById('shadow-button');
 const $shadowForm = document.getElementById('shadow-register');
+const $shadowUser = $shadowForm.querySelector('.username');
 let clientFingerprint = null;
+let shadowAction = null;
 $shadowButton.disabled = true;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const next = urlParams.has('next') ? urlParams.get('next') : '/';
 
 async function fingerprintDevice() {
     return await Fingerprint2.getPromise()
@@ -27,6 +33,7 @@ function checkShadowRecords() {
         .then(response => response.json())
         .then(json => {
             const found = json.found;
+            action = found ? 'login' : 'register';
             if (found) handleShadowButton(found, username=json.username);
             else handleShadowButton(found);
         });
@@ -36,7 +43,8 @@ function checkShadowRecords() {
 function handleShadowButton(found, username=null){
     $shadowButton.disabled = false;
     if (found) {
-        $shadowForm.querySelector('.username').value = username;
+        $shadowUser.disabled = true;
+        $shadowUser.value = username;
         $shadowButton.innerText = `${username} olarak devam et`;
     } else {
         
@@ -45,12 +53,18 @@ function handleShadowButton(found, username=null){
 
 $shadowButton.onclick = (e) => {
     e.preventDefault();
+    if (!action) return;
     const fd = new FormData($shadowForm);
-    fd.append('action', 'shadow-register');
+    fd.append('action', `shadow-${action}`);
     fd.append('fingerprint', clientFingerprint);
     fetch('', {
         method: 'POST',
         body: fd,
+    }).then(response => {
+        if (response.ok) window.location.href = next;
+        else {
+            // bir şeyler ters gitti
+        };
     })
 }
 
