6450f84584735e616e6ddeab062373b17893b268
divisia <omrfyyz@gmail.com>
2020-08-17 07:13:14 +0300

    small imp

--- gatewall/urls.py
+++ gatewall/urls.py
@@ -4,7 +4,5 @@ from .views import LoginViews, Auth
 
 app_name = 'gatewall'
 urlpatterns = [
-    path('ben-kimim/', LoginViews.Login.as_view(), name='login'),
-    path('seremoni/', LoginViews.Register.as_view(), name='register'),
-    path('tc/', Auth.as_view(), name='tc')
+    path('ben-kimim/', Auth.as_view(), name='auth')
 ]
