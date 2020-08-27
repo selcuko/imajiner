46d780d7f54b8d0d7dfc3a6aa0c0fd26711aebd4
divisia <omrfyyz@gmail.com>
2020-08-27 23:19:33 +0300

    navbar

--- imajiner/urls.py
+++ imajiner/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static, serve
+from explore.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', Home.as_view(), name='landing'),
     path('', include('notebook.urls', namespace='narrative')),
     path('', include('gatewall.urls', namespace='gatewall')),
     path('', include('explore.urls', namespace='explore')),
