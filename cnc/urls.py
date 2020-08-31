1386f46c6b89e45a01196f2816fd6ce2fb90b17a
divisia <omrfyyz@gmail.com>
2020-08-31 17:10:23 +0300

    cleaned

--- cnc/urls.py
+++ cnc/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 app_name = 'cnc'
 urlpatterns = [
-    path('komuta', CommandAndControl.as_view(), name='panel'),
+    path('komuta/', CommandAndControl.as_view(), name='panel'),
 ]
