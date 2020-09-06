e8c88923660f8a52b33c7eb4b6659fa7b4759d22
divisia <omrfyyz@gmail.com>
2020-09-06 18:09:26 +0300

    taglib initial popup (modal)

--- notebook/static/notebook/tagging.js
+++ notebook/static/notebook/tagging.js
@@ -1,36 +1,19 @@
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+const taglib = {};
+
+const $e = {};
+$e.add = document.getElementById('tag-add');
+$e.display = new Array(document.querySelectorAll('.tag-display'));
+$e.form = new FormData(document.getElementById('template-vars'));
+
+$e.add.onclick = (e) => {
+    e.preventDefault();
+    console.log(`Add slug on ${$e.form.get('narrative-slug')}`);
+};
+
+$e.display.map( (display) => {
+    display.onclick = (e) => {
+        e.preventDefault();
+        console.log(`Interact [${e.target.id}] on ${$e.form.get('narrative-slug')}`);
+
     }
-    return cookieValue;
-}
-
-function tagClick(e){
-    console.log(e);
-}
-
-
-function tagPost(slug){
-    slug =slug;
-    let data = new FormData();
-    let payload = {};
-    payload[slug] = 1;
-    data.append('payload', JSON.stringify(payload));
-    data.append('action', 'TAGDELTA')
-    data.append('csrfmiddlewaretoken', getCookie('csrftoken'));
-
-    fetch('', {
-        credentials: 'same-origin', 
-        method:"POST", 
-        body: data,
-    })
-}
+})
