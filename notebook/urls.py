2226957841f95d9b3d48ab80eac6ca3acfdb906f
divisia <omrfyyz@gmail.com>
2020-09-15 18:30:06 +0300

    notebook views splitted

--- notebook/urls.py
+++ notebook/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from django.views.generic import RedirectView
-from .views import NarrativeView, NarrativeFactory, SoundUpload
+from .views import *
 
 app_name = 'notebook'
 urlpatterns = [
-    path('hikaye/', NarrativeView.List.as_view(), name='list'),
+    path('hikaye/', NarrativeList.as_view(), name='list'),
     path('hikayeler/', RedirectView.as_view(pattern_name='notebook:list', permanent=True)),
 
-    path('hikaye/yaz/', NarrativeFactory.Write.as_view(), name='write'),
-    path('hikaye/yaz/yeni/', NarrativeFactory.New.as_view(), name='new'),
-    path('hikaye/yaz/<slug:slug>/', NarrativeFactory.Sketch.as_view(), name='sketch'),
+    path('hikaye/yaz/', NarrativeWrite.as_view(), name='write'),
+    path('hikaye/yaz/yeni/', NarrativeFolder.as_view(), name='new'),
+    path('hikaye/yaz/<slug:slug>/', NarrativeSketch.as_view(), name='sketch'),
 
-    path('hikaye/<slug:slug>/', NarrativeView.Detail.as_view(), name='detail'),
+    path('hikaye/<slug:slug>/', NarrativeDetail.as_view(), name='detail'),
 
     path('ses/', SoundUpload.as_view(), name='upload'),
 ]
--- notebook/views.py
+++ notebook/views.py
@@ -9,196 +9,194 @@ import uuid
 from django.core.exceptions import SuspiciousOperation
 
 
-class NarrativeView:
-    class Detail(DetailView):
-        model = Narrative
-        context_object_name = 'narrative'
-        template_name = 'notebook/narrative/detail.html'
+class NarrativeDetail(DetailView):
+    model = Narrative
+    context_object_name = 'narrative'
+    template_name = 'notebook/narrative/detail.html'
 
-        def get_object(self):
-            self.slug = self.kwargs['slug']
-            self.narrative = Narrative.objects.get(slug=self.slug)
-            return self.narrative
-        
-        def get_context_data(self, *args, **kwargs):
-            ctx = super().get_context_data(*args, **kwargs)
-            ctx.update({
-                'doc': {
-                    'title': self.narrative.title,
-                    'author': self.narrative.author.username,
-                }
-            })
-            return ctx
-        
-        def post(self, request, *args, **kwargs):
-            action = request.POST['action']
-            try:
-                if not request.user.is_authenticated: raise SuspiciousOperation('Giriş yapılmamış')
-
-                narrative = request.POST['narrative']
-                narrative = Narrative.objects.get(slug=narrative)
-
-                if action == 'tag-create':
-                    abstract = request.POST['name']
-                    try:
-                        abstract = AbstractTag.objects.get(name=abstract)
-                    except:
-                        abstract = AbstractTag.objects.create(name=abstract)
-                    narrative.tags.add(abstract)
-                    request.user.tags.delta_for(abstract=abstract, narrative=narrative, delta=1)
-                    return HttpResponse()
-                
-                elif action == 'tag-step':
-                    abstract = request.POST['name']
-                    request.user.tags.delta_for(narrative=narrative, abstract=abstract, delta=1)
-                    return HttpResponse()
-
-            except Exception as e:
-                raise e
-        
-
-        def tag_delta(self, slug, amount=1):
-            user = self.request.user
-            if not user.is_authenticated:
-                return False
-            user.tags.delta(
-                slug=slug,
-                diff=amount,
-                narrative=self.kwargs['slug']
-            )
-
-    class List(ListView):
-        model = Narrative
-        context_object_name = 'narratives'
-        template_name = 'notebook/narrative/list.html'
-        paginate_by = 12
-        ordering = ('created_at',)
-
-        def get_context_data(self, *args, **kwargs):
-            ctx = super().get_context_data(*args, **kwargs)
-            ctx.update({
-                'doc': {
-                    'title': 'Hikayeler',
-                    'author': 'Imajiner Müşterileri'
-                }
-            })
-            return ctx
-
-        def get_queryset(self):
-            return Narrative.objects.filter(sketch=False, author__isnull=False).order_by('-created_at')
-
-
-
-class NarrativeFactory:
-    class New(View):
-        template_name = 'notebook/narrative/write.html'
-
-        def get(self, request):
-            if not request.user.is_authenticated:
-                return redirect(f'{reverse("gatewall:auth")}?next={reverse("notebook:new")}&espresso')
-
-            form = NarrativeWrite()
-            form.fields['sound'].queryset = SoundRecord.objects.filter(uploader=request.user)
-            return render(self.request, self.template_name, context={'form':form})
-
-        def post(self, request):
-            if not request.user.is_authenticated:
-                return HttpResponse("Prohibited")
-            form = NarrativeWrite(request.POST)
-            action = request.POST.get('action', 'SUBMIT')
-            count = int(request.POST.get('count', 1))
-            
-
-            if not form.is_valid():
-                return render(self.request, self.template_name, context={'form':form})
-            
-            if action == 'SUBMIT':
-                if count > 0:
-                    uid = request.POST.get('uuid')
-                    uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
-                    narrative = Narrative.objects.get(uuid=uid)
-                    form = NarrativeWrite(request.POST, instance=narrative)
-                    narrative = form.save(commit=False)
-                    narrative.sketch = False
-                    narrative.save()
-                
-                else:
-                    uid = request.POST.get('uuid')
-                    uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
-                    form = NarrativeWrite(request.POST)
-                    narrative = form.save(commit=False)
-                    narrative.uuid = uid
-                    narrative.author = request.user
-                    narrative.sketch = False
-                    narrative.save()
-
-
-                narrative = form.save()
-                return redirect(narrative)
-            
-            elif action == 'AUTOSAVE':
-                
-                if count > 0:
-                    uid = request.POST.get('uuid')
-                    uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
-                    narrative = Narrative.objects.get(uuid=uid)
-                    form = NarrativeWrite(request.POST, instance=narrative)
-                    if not narrative.sketch:
-                        raise Exception('Wait a second!')
-                    form.save()
-                else:
-                    uid = request.POST.get('uuid')
-                    uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
-                    form = NarrativeWrite(request.POST)
-                    narrative = form.save(commit=False)
-                    narrative.uuid = uid
-                    narrative.sketch = True
-                    narrative.author = request.user
-                    narrative.save()
-                return HttpResponse('skec')
-            else:
-                return HttpResponse('Unknown')
-
-
-    class Write(View):
-        template_name = 'notebook/narrative/folder.html'
-        def get(self, request):
-            if not request.user.is_authenticated:
-                return redirect(f'{reverse("gatewall:auth")}?next={reverse("notebook:folder")}&espresso')
-
-            sketches = Narrative.objects.filter(sketch=True, author=request.user)
-            return render(request, self.template_name, {
-                'sketches': sketches,
-                'no_sketch': not sketches.exists(),
-            })
+    def get_object(self):
+        self.slug = self.kwargs['slug']
+        self.narrative = Narrative.objects.get(slug=self.slug)
+        return self.narrative
     
-    class Sketch(DetailView):
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        ctx.update({
+            'doc': {
+                'title': self.narrative.title,
+                'author': self.narrative.author.username,
+            }
+        })
+        return ctx
+    
+    def post(self, request, *args, **kwargs):
+        action = request.POST['action']
+        try:
+            if not request.user.is_authenticated: raise SuspiciousOperation('Giriş yapılmamış')
 
-        template_name = 'notebook/narrative/write.html'
+            narrative = request.POST['narrative']
+            narrative = Narrative.objects.get(slug=narrative)
+
+            if action == 'tag-create':
+                abstract = request.POST['name']
+                try:
+                    abstract = AbstractTag.objects.get(name=abstract)
+                except:
+                    abstract = AbstractTag.objects.create(name=abstract)
+                narrative.tags.add(abstract)
+                request.user.tags.delta_for(abstract=abstract, narrative=narrative, delta=1)
+                return HttpResponse()
+            
+            elif action == 'tag-step':
+                abstract = request.POST['name']
+                request.user.tags.delta_for(narrative=narrative, abstract=abstract, delta=1)
+                return HttpResponse()
+
+        except Exception as e:
+            raise e
+    
+
+    def tag_delta(self, slug, amount=1):
+        user = self.request.user
+        if not user.is_authenticated:
+            return False
+        user.tags.delta(
+            slug=slug,
+            diff=amount,
+            narrative=self.kwargs['slug']
+        )
+
+class NarrativeList(ListView):
+    model = Narrative
+    context_object_name = 'narratives'
+    template_name = 'notebook/narrative/list.html'
+    paginate_by = 12
+    ordering = ('created_at',)
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        ctx.update({
+            'doc': {
+                'title': 'Hikayeler',
+                'author': 'Imajiner Müşterileri'
+            }
+        })
+        return ctx
+
+    def get_queryset(self):
+        return Narrative.objects.filter(sketch=False, author__isnull=False).order_by('-created_at')
+
+
+
+class NarrativeWrite(View):
+    template_name = 'notebook/narrative/write.html'
+
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect(f'{reverse("gatewall:auth")}?next={reverse("notebook:new")}&espresso')
+
+        form = NarrativeWrite()
+        form.fields['sound'].queryset = SoundRecord.objects.filter(uploader=request.user)
+        return render(self.request, self.template_name, context={'form':form})
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return HttpResponse("Prohibited")
+        form = NarrativeWrite(request.POST)
+        action = request.POST.get('action', 'SUBMIT')
+        count = int(request.POST.get('count', 1))
         
-        def get(self, request, slug):
-            if not request.user.is_authenticated:
-                return redirect(f'{reverse("gatewall:auth")}?next={reverse("notebook:folder")}&espresso')
-            narrative = Narrative.objects.get(slug=slug, author=request.user)
-            form = NarrativeWrite(instance=narrative)
-            return render(request, self.template_name, {'form': form})
 
-        def post(self, request, slug):
-            if not request.user.is_authenticated:
-                return HttpResponse(status=403)
-            narrative = Narrative.objects.get(slug=slug)
-            form = NarrativeWrite(request.POST, instance=narrative)
-            if not form.is_valid():
-                print(form.errors)
-            action = request.POST.get('action', '')
-            narrative = form.save(commit=False)
-            submitted = action == 'SUBMIT'
-            if submitted:
+        if not form.is_valid():
+            return render(self.request, self.template_name, context={'form':form})
+        
+        if action == 'SUBMIT':
+            if count > 0:
+                uid = request.POST.get('uuid')
+                uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
+                narrative = Narrative.objects.get(uuid=uid)
+                form = NarrativeWrite(request.POST, instance=narrative)
+                narrative = form.save(commit=False)
                 narrative.sketch = False
+                narrative.save()
+            
+            else:
+                uid = request.POST.get('uuid')
+                uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
+                form = NarrativeWrite(request.POST)
+                narrative = form.save(commit=False)
+                narrative.uuid = uid
+                narrative.author = request.user
+                narrative.sketch = False
+                narrative.save()
 
-            narrative.save(alter_slug=submitted)
+
+            narrative = form.save()
             return redirect(narrative)
+        
+        elif action == 'AUTOSAVE':
+            
+            if count > 0:
+                uid = request.POST.get('uuid')
+                uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
+                narrative = Narrative.objects.get(uuid=uid)
+                form = NarrativeWrite(request.POST, instance=narrative)
+                if not narrative.sketch:
+                    raise Exception('Wait a second!')
+                form.save()
+            else:
+                uid = request.POST.get('uuid')
+                uid = uuid.UUID(bytes=bytes(uid[-16:].encode()))
+                form = NarrativeWrite(request.POST)
+                narrative = form.save(commit=False)
+                narrative.uuid = uid
+                narrative.sketch = True
+                narrative.author = request.user
+                narrative.save()
+            return HttpResponse('skec')
+        else:
+            return HttpResponse('Unknown')
 
+
+class NarrativeSketch(DetailView):
+
+    template_name = 'notebook/narrative/write.html'
+    
+    def get(self, request, slug):
+        if not request.user.is_authenticated:
+            return redirect(f'{reverse("gatewall:auth")}?next={reverse("notebook:folder")}&espresso')
+        narrative = Narrative.objects.get(slug=slug, author=request.user)
+        form = NarrativeWrite(instance=narrative)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, slug):
+        if not request.user.is_authenticated:
+            return HttpResponse(status=403)
+        narrative = Narrative.objects.get(slug=slug)
+        form = NarrativeWrite(request.POST, instance=narrative)
+        if not form.is_valid():
+            print(form.errors)
+        action = request.POST.get('action', '')
+        narrative = form.save(commit=False)
+        submitted = action == 'SUBMIT'
+        if submitted:
+            narrative.sketch = False
+
+        narrative.save(alter_slug=submitted)
+        return redirect(narrative)
+
+
+class NarrativeFolder(View):
+    template_name = 'notebook/narrative/folder.html'
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return redirect(f'{reverse("gatewall:auth")}?next={reverse("notebook:folder")}&espresso')
+
+        sketches = Narrative.objects.filter(sketch=True, author=request.user)
+        return render(request, self.template_name, {
+            'sketches': sketches,
+            'no_sketch': not sketches.exists(),
+        })
 
 
 class SoundUpload(CreateView):
