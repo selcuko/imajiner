f6974bfb1ab84455d29521cfeb4afc5996cda378
divisia <omrfyyz@gmail.com>
2020-10-09 18:44:43 +0300

    auth redirect bug fix

--- gatewall/static/gatewall.js
+++ gatewall/static/gatewall.js
@@ -2,7 +2,7 @@ const defaultRedirectPath = '/';
 
 const query = window.location.search;
 const urlParams = new URLSearchParams(query);
-const next = urlParams.has('next') ? urlParams.get('sonraki') : defaultRedirectPath;
+const next = urlParams.has('sonraki') ? urlParams.get('sonraki') : defaultRedirectPath;
 const auto = urlParams.has('espresso') ? true : false;
 
 const redirect = () => { window.location.href = next; }
