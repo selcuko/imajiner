a2b01239dadc6f571a13d2f860cae13dba38ccdc
divisia <omrfyyz@gmail.com>
2020-10-30 23:38:12 +0300

    console login required

--- console/urls.py
+++ console/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 app_name = 'console'
 urlpatterns = [
-    path('konsol/', console, name='console'),
+    path('konsol/', Console.as_view(), name='console'),
 ]
--- console/views.py
+++ console/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def console(request):
-    return render(request, 'console/overview.html', {})
+class Console(LoginRequiredMixin, View):
+    template = 'console/overview.html'
+
+    def get(self, request):
+        return render(request, self.template)
