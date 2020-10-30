a5ce1ea3adba3f8a5ad4bae072d601a8082dcb89
divisia <omrfyyz@gmail.com>
2020-10-30 22:00:32 +0300

    set allowed hosts

--- imajiner/settings.py
+++ imajiner/settings.py
@@ -13,7 +13,8 @@ ALLOWED_HOSTS = [
     'localhost',
     '192.168.1.10',
     '192.168.43.182',
-    'alpha.imajiner.space',
+    'imajiner.space',
+    'imajiner.herokuapp.com',
 ]
 
 INSTALLED_APPS = [
