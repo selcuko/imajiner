71e285bdfb096aaaf26be35d9111c60a0f36c32e
divisia <omrfyyz@gmail.com>
2020-11-18 21:19:20 +0300

    notebook url translation tags

--- notebook/urls.py
+++ notebook/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from .views import *
+from django.utils.translation import gettext_lazy as _
 
 app_name = 'notebook'
 urlpatterns = [
     path('', NarrativeList.as_view(), name='list'),
 
-    path('yaz/', NarrativeFolder.as_view(), name='folder'),
-    path('yaz/yeni/', NarrativeWrite.as_view(), name='write'),
-    path('yaz/<uuid:uuid>/', NarrativeWrite.as_view(), name='sketch'),
+    path(_('write/'), NarrativeFolder.as_view(), name='folder'),
+    path(_('write/new/'), NarrativeWrite.as_view(), name='write'),
+    path(_('write/<uuid:uuid>/'), NarrativeWrite.as_view(), name='sketch'),
 
     path('<slug:slug>/', NarrativeDetail.as_view(), name='detail'),
 ]
