484fa0aae094fdaba9ffe40caa01031fca26d114
divisia <omrfyyz@gmail.com>
2020-12-09 09:14:15 +0300

    console/views.py formatted and removed redundant

--- console/urls.py
+++ console/urls.py
@@ -12,8 +12,5 @@ urlpatterns = [
     path(_('narratives/<uuid:narrative>/'), Narrative.as_view(), name='narrative'),
     path(_('narratives/<uuid:narrative>/versions/'), NarrativeVersions.as_view(), name='versions'),
     path(_('narratives/<uuid:narrative>/versions/<uuid:version>/'), NarrativeVersion.as_view(), name='version'),
-    path(_('refereeship/'), Refereeship.as_view(), name='refereeship'),
-    path(_('studio/'), Studio.as_view(), name='studio'),
-    path(_('subscriptions/'), Subscriptions.as_view(), name='subscriptions'),
     path(_('preferences/'), Preferences.as_view(), name='preferences'),
 ]
--- console/views.py
+++ console/views.py
@@ -17,25 +17,27 @@ from django.utils.translation import gettext_lazy as _
 
 logger = logging.getLogger(__name__)
 
+
 class Overview(LoginRequiredMixin, View):
     template = 'console/overview.html'
 
     def get(self, request):
-        return render(request, self.template, {'doc': {
-        'title': _('console overview').title(),
-    }})
+        return render(request, self.template, {
+            'doc': {
+                'title': _('console overview').title(),
+            }
+        })
 
 
 class Profile(LoginRequiredMixin, View):
     template = 'console/profile.html'
-    
 
     def get(self, request):
         return render(request, self.template, {
             'form': ProfileForm(instance=request.user.profile),
             'doc':{
-        'title': _('profile').title(),
-    }
+                'title': _('profile').title(),
+            }
         })
     
     def post(self, request):
@@ -46,21 +48,24 @@ class Profile(LoginRequiredMixin, View):
             logger.warn('ProfileForm not valid.')
             
         return render(request, self.template, {
-                'form': form,
-                'doc': {
-        'title': _('profile').title(),
-    }
-            })
+            'form': form,
+            'doc': {
+                'title': _('profile').title(),
+            }
+        })
+
 
 class AccessSecurity(LoginRequiredMixin, View):
     template = 'console/access.html'
+
     def get(self, request):
-        return render(request, self.template, {'doc': {
-        'title': _('access & security').title(),
-    }})
+        return render(request, self.template, {
+            'doc': {
+                'title': _('access & security').title(),
+            }
+        })
     
     def post(self, request):
-        print(request.POST)
         if 'basics' in request.POST:
             username = request.POST.get('username', None)
             password = request.POST.get('password', None)
@@ -69,29 +74,42 @@ class AccessSecurity(LoginRequiredMixin, View):
                 request.user.save()
             if password:
                 request.user.set_password(password)
+
         elif 'session_key' in request.POST:
             try: Session.objects.get(session_key=request.POST['session_key']).delete()
             except Session.DoesNotExist: pass
-        return render(request, self.template, {'doc': {
-        'title': _('access & security').title(),
-    }})
+
+        return render(request, self.template, {
+            'doc': {
+                'title': _('access & security').title(),
+            }
+        })
+
 
 class Narratives(LoginRequiredMixin, View):
     template = 'console/narratives.html'
+
     def get(self, request):
-        return render(request, self.template, {'doc': {
-        'title': _('my narratives').capitalize(),
-    }})
-        
+        return render(request, self.template, {
+            'doc': {
+                'title': _('my narratives').capitalize(),
+            }
+        })
+
+
 class Narrative(LoginRequiredMixin, View):
     template = 'console/narrative.html'
+
     def get(self, request, narrative):
         narrative = NarrativeModel.objects.get(author=request.user, uuid=narrative)
         form = NarrativeForm(instance=narrative)
-        return render(request, self.template, {'form': form,'narrative': narrative,
-        'doc':{
-        'title': _('edit narrative').capitalize(),
-    }})
+        return render(request, self.template, {
+            'form': form,
+            'narrative': narrative,
+            'doc':{
+                'title': _('edit narrative').capitalize(),
+            }
+        })
     
     def post(self, request, narrative):
         narrative = NarrativeModel.objects.get(author=request.user, uuid=narrative)
@@ -99,93 +117,67 @@ class Narrative(LoginRequiredMixin, View):
         if form.is_valid(): 
             narrative = form.save(commit=False)
             narrative.save(new_version=True)
-        else:
-            print('FORM INVALID')
-            print(form.errors)
         return render(request, self.template, {
+            'form': form,'narrative': narrative,
             'doc': {
-        'title': _('edit narrative').capitalize(),
-    }, 'form': form,'narrative': narrative})
+                'title': _('edit narrative').capitalize(),
+            }
+        })
+
 
 class NarrativeVersions(LoginRequiredMixin, View):
     template = 'console/narrative/versions.html'
+
     def get(self, request, narrative):
         narrative = NarrativeModel.objects.get(uuid=narrative)
-        return render(request, self.template, {'doc':{
-        'title': _('narrative version timeline').capitalize(),
-    }, 'narrative': narrative})
+        return render(request, self.template, {
+            'narrative': narrative,
+            'doc':{
+                'title': _('narrative version timeline').capitalize(),
+            }
+        })
 
 class NarrativeVersion(LoginRequiredMixin, View):
     template = 'console/narrative/readonly.html'
+
     def get(self, request, narrative, version):
         version = NarrativeVersionModel.objects.get(uuid=version)
-        return render(request, self.template, {'doc': {
-        'title': _('narrative version history').capitalize(),
-    }, 'version': version})
-
-class Sketches(LoginRequiredMixin, View):
-    template = 'console/blank.html'
-    def get(self, request):
-        return render(request, self.template, {'doc': {
-        'title': _('console').capitalize(),
-    }})
-class Sketch(LoginRequiredMixin, View):
-    template = 'console/blank.html'
-    def get(self, request):
-        return render(request, self.template, {'doc': {
-        'title': _('console').capitalize(),
-    }})
+        return render(request, self.template, {
+            'version': version,
+            'doc': {
+                'title': _('narrative version history').capitalize(),
+            }
+        })
 
 
-
-class Subscriptions(LoginRequiredMixin, View):
-    template = 'console/blank.html'
-    document = {
-        'title': _('console').capitalize(),
-    }
-    def get(self, request):
-        return render(request, self.template, {'doc': self.document})
 
 class Preferences(LoginRequiredMixin, View):
     template = 'console/preferences.html'
     lang_codes = set(settings.LANGUAGES_DICT.keys())
-    document = {
-        'title': _('console').capitalize(),
-    }
 
     def get(self, request):
-        return render(request, self.template, {'settins': settings})
+        return render(request, self.template, {
+            'settins': settings,
+            'doc': {
+                'title': _('preferences').capitalize(),
+            }
+        })
     
     def post(self, request):
         action = request.POST.get('action', None)
-        if not action: raise SuspiciousOperation
 
         if action == 'set-languages':
             languages = set(request.POST.get('language-codes').split(','))
             
             if not languages.issubset(self.lang_codes): 
+                #SUS language options are predefined in settings.py
                 raise SuspiciousOperation
+
             request.user.profile.languages = list(languages)
             request.user.profile.save()
         
         else:
-            raise SuspiciousOperation('action is not supplied in POST or recognized.')
+            raise SuspiciousOperation('Action is not supplied in POST or recognized.')
 
         return HttpResponse()
-
-class Studio(LoginRequiredMixin, View):
-    template = 'console/blank.html'
-    document = {
-        'title': _('console').capitalize(),
-    }
-    def get(self, request):
-        return render(request, self.template, {'doc': self.document})
-
-class Refereeship(LoginRequiredMixin, View):
-    template = 'console/blank.html'
-    document = {
-        'title': _('console').capitalize(),
-    }
-    def get(self, request):
-        return render(request, self.template, {'doc': self.document})
 
