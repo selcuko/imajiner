f9538b1fd10a4e02b228628c21ca3cac8427a115
divisia <omrfyyz@gmail.com>
2020-12-12 13:51:26 +0300

    notebook.managers docstrings

--- notebook/managers.py
+++ notebook/managers.py
@@ -4,6 +4,13 @@ from django.db import models
 class NarrativePublicity(models.Manager):
 
     def public(self, *args, **kwargs):
+        """Returns all publicly browsable instanes
+        of given QuerySet.
+
+        Returns:
+            QuerySet: Publicly viewable instances
+        """
+
         visitor = kwargs.pop('visitor', None)
         qs = super().filter(
             sketch=False,
@@ -12,15 +19,48 @@ class NarrativePublicity(models.Manager):
             body__isnull=False)
         return qs
     
+
     def sketches(self, *args, **kwargs):
+        """Returns sketches that belongs to a particular author.
+
+        Args:
+            author (auth.User): Author
+
+        Returns:
+            QuerySet: Sketches of the author
+        """
+
         author = kwargs.pop('author')
         qs = super().filter(
             master__author=author,
             sketch=True,
         )
-        # qs.filter(title__isnull=True, body__isnull=True).delete()
         return qs
     
+    
     def sketch(self, *args, **kwargs):
+        """Returns a particular sketch of an author.
+
+        Args:
+            author (auth.User): Author of the sketch
+            uuid (UUID): UUID of sketch to be returned
+
+        Returns:
+            NarrativeTranslation: Sketch of that author
+        """
+
         uuid = kwargs.pop('uuid')
         return self.sketches(*args, **kwargs).get(uuid=uuid)
+    
+
+    def of(self, *args, **kwargs):
+        """All public narratives of an author.
+
+        Args:
+            author (auth.User): The author
+
+        Returns:
+            QuerySet: All published narratives of the author
+        """
+        author = kwargs.pop('author')
+        return self.public(*args, **kwargs).filter(master__author=author)
